33b35bcd18bb81946ca48fe8995d5a49b55abfee
Priya Bargota <zcabarg@ucl.ac.uk>
2025-11-18 21:47:30 +0000

    refactor: Remove placeholder Lesson entity

--- android/app/src/main/java/com/manuscripta/student/data/local/ManuscriptaDatabase.java
+++ android/app/src/main/java/com/manuscripta/student/data/local/ManuscriptaDatabase.java
@@ -3,14 +3,13 @@ package com.manuscripta.student.data.local;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import com.manuscripta.student.data.model.Lesson;
 import com.manuscripta.student.data.model.MaterialEntity;
 
 /**
  * Room database for Manuscripta application.
  * This is the main database configuration.
  */
-@Database(entities = {Lesson.class, MaterialEntity.class}, version = 2, exportSchema = false)
+@Database(entities = {MaterialEntity.class}, version = 3, exportSchema = false)
 public abstract class ManuscriptaDatabase extends RoomDatabase {
 
     /**
