5a39fc5526f953cb318c930980d17c21c076f01f
William Stephen <zcabwst@ucl.ac.uk>
2025-11-18 22:54:43 +0000

    Test: Add unit tests for enums
    Add unit tests for the `MaterialType` and `QuestionType` enums.
    The tests verify the correctness of the `getDisplayName()` and `requiresOptions()` methods.

---
+++ android/app/src/test/java/com/manuscripta/student/data/model/MaterialTypeTest.java
@@ -0,0 +1,19 @@
+package com.manuscripta.student.data.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link MaterialType} enum.
+ */
+public class MaterialTypeTest {
+
+    @Test
+    public void testGetDisplayName() {
+        assertEquals("Reading Material", MaterialType.LESSON.getDisplayName());
+        assertEquals("Quiz", MaterialType.QUIZ.getDisplayName());
+        assertEquals("Worksheet", MaterialType.WORKSHEET.getDisplayName());
+        assertEquals("Poll", MaterialType.POLL.getDisplayName());
+    }
+}
