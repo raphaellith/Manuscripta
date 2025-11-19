504a2cd3c60452aeea7c319fcd2ebdad0a448b09
William Stephen <zcabwst@ucl.ac.uk>
2025-11-19 11:49:44 +0000

    test(MaterialEntity): Add constructor and getter tests
    Expands the `MaterialEntityTest` to verify the functionality of the constructor and getter methods. The setup now initializes `MaterialEntity` with specific values, and the test asserts that the getters return these expected values.

--- android/app/src/test/java/com/manuscripta/student/data/model/MaterialEntityTest.java
+++ android/app/src/test/java/com/manuscripta/student/data/model/MaterialEntityTest.java
@@ -1,5 +1,8 @@
 package com.manuscripta.student.data.model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,16 +14,25 @@ public class MaterialEntityTest {
 
     @Before
     public void setUp() {
-        this.materialEntity = new MaterialEntity();
+        this.materialEntity = new MaterialEntity(
+                "id",
+                MaterialType.LESSON,
+                "Title",
+                "Content",
+                "Metadata",
+                "Vocab",
+                1L
+        );
     }
 
     @Test
-    public void testConstructorAndGetters(){
-
+    public void testConstructorAndGetters() {
+        assertEquals("id", materialEntity.getId());
+        assertEquals(MaterialType.LESSON, materialEntity.getType());
+        assertEquals("Title", materialEntity.getTitle());
+        assertEquals("Content", materialEntity.getContent());
+        assertEquals("Metadata", materialEntity.getMetadata());
+        assertEquals("Vocab", materialEntity.getVocabularyTerms());
+        assertEquals(1L, materialEntity.getTimestamp());
     }
-
-
-
-
-
 }
