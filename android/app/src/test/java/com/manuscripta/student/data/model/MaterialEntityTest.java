2368bfd3f8d80f229264ff923587e9fad8a5178d
William Stephen <zcabwst@ucl.ac.uk>
2025-11-19 11:22:15 +0000

    feat: Add initial unit test for MaterialEntity
    Adds a new test file, `MaterialEntityTest.java`, with a basic setup and a placeholder test method for the `MaterialEntity` data model.

---
+++ android/app/src/test/java/com/manuscripta/student/data/model/MaterialEntityTest.java
@@ -0,0 +1,26 @@
+package com.manuscripta.student.data.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link MaterialEntity} entity.
+ */
+public class MaterialEntityTest {
+    private MaterialEntity materialEntity;
+
+    @Before
+    public void setUp() {
+        this.materialEntity = new MaterialEntity();
+    }
+
+    @Test
+    public void testConstructorAndGetters(){
+
+    }
+
+
+
+
+
+}
