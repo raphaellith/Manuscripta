edc3c0084a9c4611e14ff7e399f0889691c22acd
William Stephen <zcabwst@ucl.ac.uk>
2025-11-25 09:25:49 +0000

    feat: Add delete method to DeviceStatusDao and improve DeviceStatusEntity formatting

--- android/app/src/main/java/com/manuscripta/student/data/local/DeviceStatusDao.java
+++ android/app/src/main/java/com/manuscripta/student/data/local/DeviceStatusDao.java
@@ -1,6 +1,7 @@
 package com.manuscripta.student.data.local;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -64,7 +65,7 @@ public interface DeviceStatusDao {
      *
      * @param deviceStatus The device status entity to delete
      */
-    @androidx.room.Delete
+    @Delete
     void delete(DeviceStatusEntity deviceStatus);
 
     /**
--- android/app/src/main/java/com/manuscripta/student/data/model/DeviceStatusEntity.java
+++ android/app/src/main/java/com/manuscripta/student/data/model/DeviceStatusEntity.java
@@ -98,6 +98,7 @@ public class DeviceStatusEntity {
         }
         this.batteryLevel = batteryLevel;
     }
+
     public String getCurrentMaterialId() {
         return currentMaterialId;
     }
