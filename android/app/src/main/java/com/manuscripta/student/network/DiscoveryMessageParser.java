bc81bc8f47adf9bfe64886b443f38ef96e1560b2
William Stephen <zcabwst@ucl.ac.uk>
2025-12-02 21:13:16 +0000

    Doc: Add Javadoc to DiscoveryMessageParser
    Added Javadoc to the `DiscoveryMessageParser` class and its `parse` method. The documentation includes the protocol reference for the discovery message format.

--- android/app/src/main/java/com/manuscripta/student/network/DiscoveryMessageParser.java
+++ android/app/src/main/java/com/manuscripta/student/network/DiscoveryMessageParser.java
@@ -1,11 +1,32 @@
 package com.manuscripta.student.network;
 
 import androidx.annotation.NonNull;
-
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
+/**
+ * DiscoveryMessageParser is a class that parses a discovery message.
+ * 
+ * <pre>
+ * Protocol Reference (API Contract Section 3.3):
+ * +------------+--------+--------+------------------------------------------------+
+ * | Field      | Offset | Size   | Description                                    |
+ * +------------+--------+--------+------------------------------------------------+
+ * | Opcode     | 0      | 1 byte | 0x00 = DISCOVERY                               |
+ * | IP Address | 1      | 4 bytes| IPv4 address (network byte order, big-endian)  |
+ * | HTTP Port  | 5      | 2 bytes| Unsigned, little-endian                        |
+ * | TCP Port   | 7      | 2 bytes| Unsigned, little-endian                        |
+ * +------------+--------+--------+------------------------------------------------+
+ * </pre>
+ * 
+ * @Author William Stephen
+ */
 public class DiscoveryMessageParser {
+    /**
+     * Parses a discovery message.
+     * @param data The discovery message to parse.
+     * @return The parsed discovery message.
+     */
     @NonNull
     public static DiscoveryMessage parse(@NonNull byte[] data){
         // Validate length
@@ -31,7 +52,8 @@ public class DiscoveryMessageParser {
         buffer.order(ByteOrder.LITTLE_ENDIAN);
         int httpPort = buffer.getShort() & 0xFFFF;
         int tcpPort = buffer.getShort() & 0xFFFF;
-
+        
+        // Return the parsed discovery message
         return new DiscoveryMessage(
                 ipAddress,
                 httpPort,
