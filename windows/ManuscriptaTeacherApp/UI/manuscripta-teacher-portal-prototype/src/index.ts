0c650732c6eda6d4cde534b2185c36e9c02f366a
raphaellith <raphaellith@gmail.com>
2025-12-10 23:08:26 +0000

    closed dev tools in mainWindow

--- windows/ManuscriptaTeacherApp/UI/manuscripta-teacher-portal-prototype/src/index.ts
+++ windows/ManuscriptaTeacherApp/UI/manuscripta-teacher-portal-prototype/src/index.ts
@@ -41,8 +41,8 @@ const createWindow = (): void => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // Open the DevTools (if needed)
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
