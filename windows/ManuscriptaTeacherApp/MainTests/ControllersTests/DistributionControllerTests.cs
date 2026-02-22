467c31e5261cf0315890fc17320b74295a5cf210
raphaellith <raphaellith@gmail.com>
2026-02-22 13:28:32 +0000

    fix: merge conflicts

--- windows/ManuscriptaTeacherApp/MainTests/ControllersTests/DistributionControllerTests.cs
+++ windows/ManuscriptaTeacherApp/MainTests/ControllersTests/DistributionControllerTests.cs
@@ -208,13 +208,16 @@ public class DistributionControllerTests
 /// - s1A(3): Polymorphic entities serialised as flat composition-like structures
 /// </summary>
 public class DistributionControllerSerialisationTests : IClassFixture<CustomWebApplicationFactory>
+public class DistributionControllerSerialisationTests : IClassFixture<TestWebApplicationFactory>
 {
     private readonly CustomWebApplicationFactory _factory;
+    private readonly TestWebApplicationFactory _factory;
     private readonly Guid _testDeviceId = Guid.NewGuid();
     private readonly Guid _testMaterialId = Guid.NewGuid();
     private readonly Guid _testLessonId = Guid.NewGuid();
 
     public DistributionControllerSerialisationTests(CustomWebApplicationFactory factory)
+    public DistributionControllerSerialisationTests(TestWebApplicationFactory factory)
     {
         _factory = factory;
     }
